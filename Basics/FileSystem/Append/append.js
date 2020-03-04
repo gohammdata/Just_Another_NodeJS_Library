3438b039b300cd3d390749e2db0a0ba5287362b6
John Hamm <hammdata@outlook.com>
2020-03-03 20:05:25 -0500

    New file via fs.open

--- Basics/FileSystem/Append/append.js
+++ Basics/FileSystem/Append/append.js
@@ -1,6 +1,11 @@
 var fs = require('fs');
-
-fs.appendFile('newFile.txt', 'New Content for File!', function(err) {
+///////////////////////////
+//fs.appendFile('newFile.txt', 'New Content for File!', function(err) {
+//  if (err) throw err;
+  //console.log('New Content Saved!');
+//});
+//////////////////////////
+fs.open('newFile2.txt', 'w', function (err, file) {
   if (err) throw err;
-  console.log('New Content Saved!');
-})
+  console.log('New Empty File Created');
+});
