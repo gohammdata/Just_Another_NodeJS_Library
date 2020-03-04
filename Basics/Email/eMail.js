6e1a583199dbb93983fd53604fe7083ccf2b8bdb
John Hamm <hammdata@outlook.com>
2020-03-03 21:14:47 -0500

    Sending Emails with Node.js

---
+++ Basics/Email/eMail.js
@@ -0,0 +1,24 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'example123@gmail.com',
+    pass: 'examplepassword'
+  }
+});
+
+var mailOptions = {
+  from: 'example123@gmail.com',
+  to: 'betterexample123@yahoo.com, bestexample123@outlook.com',
+  subject: 'Sending Email using Node.js',
+  text: 'Node.js Email successful'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error){
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+})
