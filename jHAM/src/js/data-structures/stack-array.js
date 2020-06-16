61bae7f2dacdeece73e6f8349718bcbb5e8db4e5
John Hamm <gohammdata@gmail.com>
2020-06-16 11:37:58 -0400

    pop and peek method

--- jHAM/src/js/data-structures/stack-array.js
+++ jHAM/src/js/data-structures/stack-array.js
@@ -3,8 +3,19 @@ class Stack {
     constructor() {
         this.items = []; // {1}
     }
+
     //  Method to add element to top of the stack
     push(element) {
-        this.items.push(element); 
+        this.items.push(element); // push from JavaScript Array class
+    }
+
+    //  Remove last item from the Stack (LIFO)
+    pop() {
+       return this.items.pop(); //  pop from JavaScript Array class
+    }
+
+    //  Show the last element added to the stack
+    peek() {
+        return this.items[this.items.length - 1]; //    Last element is at position (length-1)
     }
 }
