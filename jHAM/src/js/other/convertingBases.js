522338f66b71697423b7b3cf83dea84169adf888
John Hamm <gohammdata@gmail.com>
2020-06-16 14:01:50 -0400

    baseConversion for all bases between 2 and 36

--- jHAM/src/js/other/convertingBases.js
+++ jHAM/src/js/other/convertingBases.js
@@ -26,3 +26,32 @@ function decimalToBinary(decimalNumber) {
 console.log(decimalToBinary(10)); // 1010 - expected
 console.log(decimalToBinary(1000)); //  1111101000 - expected
 console.log(decimalToBinary(233)); //    11101001 - expected
+
+
+//  Conversion algorithm for to convert a decimal number to all bases from 2 -> 36
+
+function baseConverter(decimalNumber, base) {
+    const remStack = new Stack();
+    const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+    let number = decNumber;
+    let rem;
+    let baseString = '';
+    if (!(base >= 2 && base <= 36)) {
+        return '';
+    }
+    while (number > 0) {
+        rem = Math.floor(number % base);
+        remStack.push(rem);
+        number = Math.floor(number / base);
+    }
+    while (!remStack.isEmpty()) {
+        baseString += digits[remStack.pop()]; //    Pop was returning the element as well
+    }
+    return baseString;
+}
+
+//  Internal Testing
+console.log(baseConverter(100345,2)); // 11000011111111001
+console.log(baseConverter(100345, 8)); // 303771
+console.log(baseConverter(100345, 16)); // 187F9
+console.log(baseConverter(100345,  35)); // 2BW0
