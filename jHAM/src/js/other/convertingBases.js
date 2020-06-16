70ff12a2dd5e870c790b7727a58eb526f5cbf5ab
John Hamm <gohammdata@gmail.com>
2020-06-16 13:30:17 -0400

    decimal to binary algorithm with custom stack

---
+++ jHAM/src/js/other/convertingBases.js
@@ -0,0 +1,8 @@
+import { Stack } from '../data-structures/stack';
+
+function decimalToBinary(decimalNumber) {
+    const remStack = new Stack();
+    let number = decimalNumber;
+    let rem;
+    let binaryString = '';
+}
