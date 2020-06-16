e1d86b8d0a925738d3daba992290f124f4208aa2
John Hamm <gohammdata@gmail.com>
2020-06-16 13:42:20 -0400

    conversion algorithm using Custom Stack Object

--- jHAM/src/js/other/convertingBases.js
+++ jHAM/src/js/other/convertingBases.js
@@ -1,8 +1,23 @@
 import { Stack } from '../data-structures/stack';
 
+
+// Applying the Object Based Stack Data Structure I just Built
+//  via decimalToBinary conversion of the Stack.
 function decimalToBinary(decimalNumber) {
     const remStack = new Stack();
     let number = decimalNumber;
-    let rem;
+    let rem; //remainder
     let binaryString = '';
+
+    while(number > 0) {
+        rem = Math.floor(number % 2);
+        remStack.push(rem);
+        number = Math.floor(number / 2);
+    }
+
+    while(!remStack.isEmpty()) {
+        binaryString += remStack.pop().toString();
+    }
+
+    return binaryString;
 }
