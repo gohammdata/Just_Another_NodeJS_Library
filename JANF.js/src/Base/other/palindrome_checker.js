261077301cee826154f7cf3a02163dba43b16d0c
gohammdata <gohammdata@gmail.com>
2020-08-10 08:51:03 -0400

    Checking the palindrome checker

--- JANF.js/src/Base/other/palindrome_checker.js
+++ JANF.js/src/Base/other/palindrome_checker.js
@@ -1,9 +1,11 @@
+import { Deque } from '../data-structures/queue_deque/deque';
+
 function palindromeChecker (aString) {
     if (aString === undefined || aString === null ||
         (aString !== null && aString.length === 0)) { // Verify if the String is valid before the algorithm logic
             return false;
         }
-        const deque = new deque();
+        const deque = new Deque();
         const lowerString = aString.toLocaleLowerCase().split(' ').join('');
         let isEqual = true;
         let firstChar, lastChar;
@@ -22,3 +24,6 @@ function palindromeChecker (aString) {
         
         return isEqual;
     }
+
+    console.log('a', palindromeChecker('a')); //    Truu
+    console.log('aa', palindromeChecker('aa')); //  Truest
