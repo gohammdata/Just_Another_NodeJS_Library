0949970e853d1802114a8fe5121276ee8d392a76
gohammdata <gohammdata@gmail.com>
2020-08-10 08:54:38 -0400

    Node model for linked list

---
+++ JANF.js/src/Base/data-structures/linked-lists/models/linked-list-models.js
@@ -0,0 +1,7 @@
+//  Node class that represents the item that is added to the list
+export class Node {
+    constructor(element) {
+        this.element = element;
+        this.next = undefined;
+    }
+}
